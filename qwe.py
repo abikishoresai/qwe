78386cb240536f84c8cf9587a830316aaf4cd0a8
abikishoresai <52270874+abikishoresai@users.noreply.github.com>
2019-07-30 15:28:22 +0530

    Create qwe.py

---
+++ qwe.py
@@ -0,0 +1,11 @@
+int round(int n) 
+{ 
+    // Smaller multiple 
+    int a = (n / 10) * 10; 
+      
+    // Larger multiple 
+    int b = a + 10; 
+  
+    // Return of closest of two 
+    return (n - a > b - n)? b : a; 
+} 
